22fb5287aedf1f3293569d5c3b80ba0418ed68af
Aeonia <anna.cathelineau@gmail.com>
2018-04-08 19:30:38 +0200

    bug form reload fixed

--- js/opendata.js
+++ js/opendata.js
@@ -43,65 +43,74 @@ req.send(null);
 
 
 
-$("#oeuvre").one("click", function oeuvres () {
+$("#oeuvre").one("click", function oeuvres() {
+
+            var nom = document.getElementById("nom").value;
+
+            if (nom == "") {
+                alert("Vous devez rentrer votre prénom pour que la formule fonctionne ;)");
+                location.reload(); 
+            } else {
+
+            var number = Math.floor(Math.random() * (100 - 80 + 1) ) + 80;
+            var percent = document.createElement("P");                       // Create a <p> element
+            var name = document.createTextNode(nom+", vous et votre oeuvre êtes compatibles à : "+number+"%");      // Create a text node
+            percent.appendChild(name);                                          // Append the text to <p>
+            document.getElementById("mon_oeuvre").appendChild(percent); 
 
 
-    var number = Math.floor(Math.random() * (100 - 80 + 1) ) + 80;
-    var nom = document.getElementById("nom").value;
-    var percent = document.createElement("P");                       // Create a <p> element
-    var name = document.createTextNode(nom+", vous et votre oeuvre êtes compatibles à : "+number+"%");      // Create a text node
-    percent.appendChild(name);                                          // Append the text to <p>
-    document.getElementById("mon_oeuvre").appendChild(percent); 
+            var min = 0;
+            var max = 15;
+            
+            ajaxGet("https://opendata.hauts-de-seine.fr/api/records/1.0/search/?dataset=archives-de-la-planete&sort=identifiant_fakir&facet=operateur&facet=themes&facet=lieu_retraite&rows="+max, function (reponse) {
 
+                var oeuvre = JSON.parse(reponse);
+                var index = Math.floor(Math.random()*max + 0.5); 
+                console.log(index);    
+                // Récupération de certains résultats
+                var legende = oeuvre.records[index].fields.legende_revisee;
+                var date = oeuvre.records[index].fields.date_de_prise_de_vue;
+                var technique = oeuvre.records[index].fields.procede_technique;
+                var image = oeuvre.records[index].fields.photo.id;
+                var url = "https://opendata.hauts-de-seine.fr/explore/dataset/archives-de-la-planete/files/"+image+"/300"
+                // var date = oeuvre.records;
+                // Affichage des résultats
+                // var conditionsElt = document.createElement("div");
+                // conditionsElt.textContent = "Titre :" + oeuvre +
+                //     "date :" + date;
+                // var oeuvreElt = document.getElementById("oeuvre");
+                // oeuvreElt.appendChild(conditionsElt);
+                // console.log(oeuvres);
+                var im = document.createElement("IMG");  
+                im.setAttribute("src", url);
+                im.setAttribute("width", "304");
+                im.setAttribute("height", "228");
+                im.setAttribute("alt", legende); 
+                document.getElementById("mon_oeuvre").appendChild(im);                  // Create a <p> element     // Create a text node                                        // Append the text to <p>
+                //document.getElementById("mon_oeuvre").appendChild(image); 
 
-    var min = 0;
-    var max = 15;
-ajaxGet("https://opendata.hauts-de-seine.fr/api/records/1.0/search/?dataset=archives-de-la-planete&sort=identifiant_fakir&facet=operateur&facet=themes&facet=lieu_retraite&rows="+max, function (reponse) {
+                var para = document.createElement("P");                       // Create a <p> element
+                var t = document.createTextNode(legende);      // Create a text node
+                para.appendChild(t);                                          // Append the text to <p>
+                document.getElementById("mon_oeuvre").appendChild(para);           // Append <p> to <div> with id="myDIV" 
 
-    var oeuvre = JSON.parse(reponse);
-    var index = Math.floor(Math.random()*max + 0.5); 
-    console.log(index);    
-    // Récupération de certains résultats
-    var legende = oeuvre.records[index].fields.legende_revisee;
-    var date = oeuvre.records[index].fields.date_de_prise_de_vue;
-    var technique = oeuvre.records[index].fields.procede_technique;
-    var image = oeuvre.records[index].fields.photo.id;
-    var url = "https://opendata.hauts-de-seine.fr/explore/dataset/archives-de-la-planete/files/"+image+"/300"
-    // var date = oeuvre.records;
-    // Affichage des résultats
-    // var conditionsElt = document.createElement("div");
-    // conditionsElt.textContent = "Titre :" + oeuvre +
-    //     "date :" + date;
-    // var oeuvreElt = document.getElementById("oeuvre");
-    // oeuvreElt.appendChild(conditionsElt);
-    // console.log(oeuvres);
-    var im = document.createElement("IMG");  
-    im.setAttribute("src", url);
-    im.setAttribute("width", "304");
-    im.setAttribute("height", "228");
-    im.setAttribute("alt", legende); 
-    document.getElementById("mon_oeuvre").appendChild(im);                  // Create a <p> element     // Create a text node                                        // Append the text to <p>
-    //document.getElementById("mon_oeuvre").appendChild(image); 
+                var para1 = document.createElement("P");                       // Create a <p> element
+                var t1 = document.createTextNode(date);      // Create a text node
+                para1.appendChild(t1);                                          // Append the text to <p>
+                document.getElementById("mon_oeuvre").appendChild(para1);           // Append <p> to <div> with id="myDIV" 
 
-    var para = document.createElement("P");                       // Create a <p> element
-    var t = document.createTextNode(legende);      // Create a text node
-    para.appendChild(t);                                          // Append the text to <p>
-    document.getElementById("mon_oeuvre").appendChild(para);           // Append <p> to <div> with id="myDIV" 
+                var para2 = document.createElement("P");                       // Create a <p> element
+                var t2 = document.createTextNode(technique);      // Create a text node
+                para2.appendChild(t2);                                          // Append the text to <p>
+                document.getElementById("mon_oeuvre").appendChild(para2);           // Append <p> to <div> with id="myDIV" 
 
-    var para1 = document.createElement("P");                       // Create a <p> element
-    var t1 = document.createTextNode(date);      // Create a text node
-    para1.appendChild(t1);                                          // Append the text to <p>
-    document.getElementById("mon_oeuvre").appendChild(para1);           // Append <p> to <div> with id="myDIV" 
+                $("#oeuvre").hide();
+                //document.getElementById("oeuvre").innerHTML = technique;
+                $("#question").hide();
+                
+                });
 
-    var para2 = document.createElement("P");                       // Create a <p> element
-    var t2 = document.createTextNode(technique);      // Create a text node
-    para2.appendChild(t2);                                          // Append the text to <p>
-    document.getElementById("mon_oeuvre").appendChild(para2);           // Append <p> to <div> with id="myDIV" 
-
-    $("btn").hide();
-    //document.getElementById("oeuvre").innerHTML = technique;
-    });
-  
+     }; 
 
 });
 
